3f48b1f394da3115214d8dd554a88f45b41594ed
asif10388 <asif.khan10388@gmail.com>
2021-10-02 18:19:50 +0600

    Fixed Sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,19 +24,11 @@ module.exports = {
         short_name: `asif`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        sitemapSize: 5000,
-      },
-    },
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`,
     "gatsby-plugin-postcss",
     `gatsby-plugin-sass`,
